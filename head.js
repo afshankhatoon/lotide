1b094902760657aac95a936870abe06950b40c51
afshankhatoon <afshankhatoon.job@gmail.com>
2020-10-24 22:17:07 +0000

    array

--- head.js
+++ head.js
@@ -6,8 +6,11 @@ const assertEqual = function(actual, expected) {
     console.log(`Assertion Failed: ${actual} === ${expected}`);
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual("apple","apple");
-assertEqual(1, 1);
-assertEqual(1, 2);
+function head(array){
+  return array[0];
+}
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([]),undefined);
+assertEqual(head([5]),5);
