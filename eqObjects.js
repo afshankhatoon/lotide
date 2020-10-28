1f0c72304bc5f847b9057333dd5aadb8401a2bc5
afshankhatoon <afshankhatoon.job@gmail.com>
2020-10-28 05:14:29 +0000

    third commit

--- eqObjects.js
+++ eqObjects.js
@@ -1,6 +1,6 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
-  if(actual===expected)
+  if(eqObjects(actual,expected))
     console.log(`Assertion Passed: ${actual} === ${expected}`);
   else
     console.log(`Assertion Failed: ${actual} === ${expected}`);
@@ -50,15 +50,15 @@ const eqObjects = function(object1, object2) {
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
 const abc = { a: "1", b: "2", c: "3" };
-assertEqual(eqObjects(ab, ba),true); 
-assertEqual(eqObjects(ab, abc),false);
+assertEqual(ab, ba); 
+assertEqual(ab, abc);
 
 const cd = { c: "1", d: ["2", 3] };
 const dc = { d: ["2", 3], c: "1" };
-assertEqual(eqObjects(cd, dc),true);
+assertEqual(cd, dc);
 
 
  const cd2 = { c: "1", d: ["2", 3, 4] };
 
- assertEqual(eqObjects(cd, cd2),false);
+ assertEqual(cd, cd2);
 
