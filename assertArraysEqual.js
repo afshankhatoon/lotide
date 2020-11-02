45b57d0882459be15bf25dd7c577421517eb621a
afshankhatoon <afshankhatoon.job@gmail.com>
2020-11-02 07:35:08 +0000

    w2d1 refactor

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,3 +1,4 @@
+const eqArrays = require('./eqArrays');
 const assertArraysEqual = function(actual, expected) {
   if(eqArrays(actual,expected))
     console.log(`Assertion Passed: ${actual} === ${expected}`);
@@ -5,7 +6,7 @@ const assertArraysEqual = function(actual, expected) {
     console.log(`Assertion Failed: ${actual} === ${expected}`);
 };
 
-function eqArrays(arr1,arr2){
+/* function eqArrays(arr1,arr2){
   if(arr1.length!==arr2.length)
     return false;
   else{
@@ -16,8 +17,10 @@ function eqArrays(arr1,arr2){
     }
     return true;
   }
-}
+} */
 
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
+/* assertArraysEqual([1, 2, 3], [1, 2, 3]);
 assertArraysEqual([1, 2, 3], [1, 2, "3"]);
+ */
+module.exports = assertArraysEqual;
