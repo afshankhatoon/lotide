4c6b50c69de4ffed0f7d698152162993617145c7
afshankhatoon <afshankhatoon.job@gmail.com>
2020-11-02 07:18:29 +0000

    w2d1 refactor

--- assertEqual.js
+++ assertEqual.js
@@ -1,4 +1,3 @@
-// FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if(actual===expected)
     console.log(`Assertion Passed: ${actual} === ${expected}`);
@@ -7,7 +6,21 @@ const assertEqual = function(actual, expected) {
 };
 
 // TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
+/* assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual("apple","apple");
 assertEqual(1, 1);
-assertEqual(1, 2);
+assertEqual(1, 2); */
+/* const tail = function(arr) {
+  let newArr=[];
+  for (let i = 1; i<arr.length; i++){
+    newArr.push(arr[i]);
+  }
+  return newArr;
+} */
+/* const result = tail(["Hello", "Lighthouse", "Labs"]);
+console.log(typeof result);
+const expResult = ["Lighthouse", "Labs"];
+console.log(typeof expResult);
+assertEqual(result, expResult); */
+
+module.exports = assertEqual;
