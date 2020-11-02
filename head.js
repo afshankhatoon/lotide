993df5569dc437ebdff26ae678eeb5d6320e54f0
J <J@afshans-MacBook-Pro.local>
2020-11-01 23:14:15 -0800

    w2d1 refactor

--- head.js
+++ head.js
@@ -1,16 +1,18 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
+/* const assertEqual = function(actual, expected) {
   if(actual===expected)
     console.log(`Assertion Passed: ${actual} === ${expected}`);
   else
     console.log(`Assertion Failed: ${actual} === ${expected}`);
-};
+}; */
 
-function head(array){
+
+const head=function(array){
   return array[0];
 }
 
-assertEqual(head([5,6,7]), 5);
+ /* assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 assertEqual(head([]),undefined);
-assertEqual(head([5]),5);
+assertEqual(head([5]),5); */
+
+module.exports = head;
