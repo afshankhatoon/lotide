a78c7b5e9e105f6dc1b2dd7f838d0d2a337e1d5e
J <J@afshans-MacBook-Pro.local>
2020-11-02 19:38:55 -0800

    w2d1 refactor middle

--- middle.js
+++ middle.js
@@ -1,4 +1,4 @@
-const assertArraysEqual = function(actual, expected) {
+/* const assertArraysEqual = function(actual, expected) {
   console.log("actual :: "+midArray(actual));
   console.log("expected :: "+expected);
   if(eqArrays(midArray(actual),expected))
@@ -6,8 +6,8 @@ const assertArraysEqual = function(actual, expected) {
   else
     console.log(`Assertion Failed: ${actual} === ${expected}`);
 };
-
-function midArray(arr){
+ */
+function middle(arr){
   let len=Math.floor(arr.length/2);
   let ans=[];
   if(arr.length%2===0)
@@ -16,7 +16,7 @@ function midArray(arr){
   return ans;
 }
 
-function eqArrays(arr1,arr2){
+/* function eqArrays(arr1,arr2){
   console.log("1::: "+arr1.length+" 2:::: "+arr2.length);
   if(arr1.length!==arr2.length)
     return false;
@@ -28,9 +28,23 @@ function eqArrays(arr1,arr2){
     }
     return true;
   }
-}
+} */
 
-assertArraysEqual([1, 2, 3, 4, 5], [3]);
+/* assertArraysEqual([1, 2, 3, 4, 5], [3]);
 assertArraysEqual([1, 2, 3, 4], [2, 3]);
 assertArraysEqual([1, 2, 3, 4, 5], [3, 4]);
-assertArraysEqual([1, 2, 3, 4], [2]);
+assertArraysEqual([1, 2, 3, 4], [2]); */
+
+/* const person = {
+  name: "Paul",
+  address: {
+    street: "310 W 95th",
+    city: "New York",
+    zipCode: 10027
+  }
+};
+
+person["phoneNumbers"] = ["1234567890,0987654321"];
+console.log(Object.keys(person)); */
+
+module.exports = middle;
